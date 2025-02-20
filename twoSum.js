816878f444afc76f439482cc1acac7d465d98f38
chandana-2101 <chandanamurashilli@gmail.com>
2025-02-20 21:08:02 +0530

    first commit

---
+++ twoSum.js
@@ -0,0 +1,17 @@
+//Given solution is array of integers nums and an integer target, 
+// return indices of the two numbers such that they add up to target.
+//it has exactly one solution and not use the same element twice
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice
+const nums = [2, 7, 11, 15];
+const twoSum = (nums, target) => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+twoSum(nums, 9);
