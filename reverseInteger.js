bb7a70bfb5aedafd305b5a2f5c4cfd3a039e451c
chandana-2101 <chandanamurashilli@gmail.com>
2025-02-21 20:40:00 +0530

    Added new file :reverseInteger.js

--- reverseInteger.js
+++ reverseInteger.js
@@ -1,3 +1,9 @@
+//The following solution signed 32-bit integer x, return x with its digits reversed. 
+// If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+//This environment does not allow you to store 64-bit integers (signed or unsigned).
+
+
 var reverse=function(x) {
     const sign = Math.sign(x);
     const result = sign * String(Math.abs(x)).split(''),reverse(),join('');
