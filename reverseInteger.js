6f0ccc934e7cdc342f36884f8f3a2417c4a31d2f
chandana-2101 <chandanamurashilli@gmail.com>
2025-02-21 20:31:33 +0530

    Added

---
+++ reverseInteger.js
@@ -0,0 +1,7 @@
+var reverse=function(x) {
+    const sign = Math.sign(x);
+    const result = sign * String(Math.abs(x)).split(''),reverse(),join('');
+    const limit = Math.pow(2,31);
+    if(result<=-limit || result>limit)return 0;
+    return result;
+};
