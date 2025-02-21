9355ca09895abe78164a496a69b716f1f21fe652
chandana-2101 <chandanamurashilli@gmail.com>
2025-02-21 20:30:17 +0530

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -2,7 +2,7 @@
 // return indices of the two numbers such that they add up to target.
 //it has exactly one solution and not use the same element twice
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice
+//You may assume that each input would have exactly one solution, and you may not use the same element twice
 const nums = [2, 7, 11, 15];
 const twoSum = (nums, target) => {
   for (let i = 0; i < nums.length; i++) {
