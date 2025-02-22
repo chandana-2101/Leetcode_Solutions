80032d9eaef9114e3530700ce893781922b4f9bf
chandana-2101 <chandanamurashilli@gmail.com>
2025-02-22 20:34:17 +0530

    Updated : zigZag-conversion.js

--- zigZag-conversion.js
+++ zigZag-conversion.js
@@ -1,3 +1,10 @@
+//The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows
+//P   A   H   N
+//A P L S I I G
+//Y   I   R
+//And then it should read line by line: "PAHNAPLSIIGYIR"
+//Below is the code for the problem
+
 var convert = function(s,numRows){
     if(numRows === 1 || s.length<0)return s;
 
